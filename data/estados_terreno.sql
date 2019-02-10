149423dd7a1de187d5970e881ab88949030bbd86
Angel O. Flores Torres <39410876+Aftorres02@users.noreply.github.com>
2019-02-10 18:00:33 -0500

    agregado , data, tablas y la app en apex

---
+++ data/estados_terreno.sql
@@ -0,0 +1,8 @@
+REM INSERTING into SV_ESTADOS
+SET DEFINE OFF;
+Insert into SV_ESTADOS (ID_ESTADO,DESCRIPCION) values (1,'Libre');
+Insert into SV_ESTADOS (ID_ESTADO,DESCRIPCION) values (2,'Vendido Con Titulo');
+Insert into SV_ESTADOS (ID_ESTADO,DESCRIPCION) values (3,'Separado');
+Insert into SV_ESTADOS (ID_ESTADO,DESCRIPCION) values (5,'No Habilitado');
+Insert into SV_ESTADOS (ID_ESTADO,DESCRIPCION) values (6,'Vendido Sin Titulo');
+Insert into SV_ESTADOS (ID_ESTADO,DESCRIPCION) values (4,'Socios');
---
+++ tables/delete_script.sql
@@ -0,0 +1,20 @@
+drop table sv_terreno_pagos purge;
+drop sequence sv_terreno_pago_seq;
+
+drop table sv_terreno_comentarios purge;
+drop sequence sv_terrenos_com_seq;
+
+drop table sv_terrenos purge;
+drop sequence sv_terrenos_seq; 
+
+drop table sv_propietarios purge;
+drop sequence sv_propietarios_seq;
+
+drop table sv_organizaciones purge;
+drop sequence sv_organizaciones_seq;
+
+drop table sv_manzanas purge;
+drop sequence sv_manzanas_seq;
+
+drop table sv_estados purge;
+drop sequence sv_estados_seq;
